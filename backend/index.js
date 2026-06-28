92fa2b9990ec55c2876ba65ac5ce9a9311a2c23c
Taofique <shornob8@gmail.com>
2026-06-29 01:42:27 +0600

    fix: implement react * fix inside index.js

--- backend/index.js
+++ backend/index.js
@@ -105,7 +105,7 @@ app.use(
 
 if (isProd) {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("{*path}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
