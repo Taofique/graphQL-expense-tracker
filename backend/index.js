6fc69ef54a7e140a39fe5b798e75cef88c0ce84c
Taofique <shornob8@gmail.com>
2026-06-29 01:46:30 +0600

    fix: fix cors origin setup with render url

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const isProd = process.env.NODE_ENV === "production";
 app.use(
   cors({
     origin: isProd
-      ? "https://your-app.onrender.com" // replace after first deploy
+      ? "https://graphql-expense-tracker-qthm.onrender.com"
       : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
