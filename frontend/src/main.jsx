c509381293fca137ad74ceb08c5537a4d34b62ea
Taofique <shornob8@gmail.com>
2026-06-29 01:54:18 +0600

    fix: fix apollo client render deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
-import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri:
+      import.meta.env.MODE === "production"
+        ? "/graphql"
+        : "http://localhost:4000/graphql",
     credentials: "include",
   }),
-  cache: new InMemoryCache(), // Apollo client uses this to cache query results after fetching them
-  // Apollo client sends cookies along with every request to the server.
+  cache: new InMemoryCache(),
 });
 
 createRoot(document.getElementById("root")).render(
